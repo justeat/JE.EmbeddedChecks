85f3268f3fc246f58749a6022db049fd748b4fb9
Peter Mounce <peter.mounce@just-eat.com>
2015-07-24 15:13:31 +0100

    yup, not yet...

--- src/JE.EmbeddedChecks.StatsD/PublishToStatsD.cs
+++ src/JE.EmbeddedChecks.StatsD/PublishToStatsD.cs
@@ -16,18 +16,18 @@ namespace JE.EmbeddedChecks.StatsD
 
         public override void PublishCheckResult(CheckResult result)
         {
-            _statsd.Timing(result.Duration, $"checks.{result.Name}.{result.Status}");
+            _statsd.Timing(result.Duration, string.Format("checks.{0}.{1}", result.Name, result.Status));
         }
 
         public override void OnCheckError(CheckResult result, Exception exception)
         {
-            _statsd.Timing(result.Duration, $"checks.{result.Name}.{result.Status}.{exception.GetType().Name}");
+            _statsd.Timing(result.Duration, string.Format("checks.{0}.{1}.{2}", result.Name, result.Status, exception.GetType().Name));
         }
 
         public override void PublishRunFinished(IList<CheckResult> results, TimeSpan elapsed)
         {
             var overall = results.All(x => x.Status == CheckStatus.Passed) ? "pass" : "errors";
-            _statsd.Timing(elapsed, $"checks._run.{overall}");
+            _statsd.Timing(elapsed, string.Format("checks._run.{0}", overall));
         }
     }
 }
